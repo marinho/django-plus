5736f39b478b25117240e4fab8de0c7dedf23dbf
Marinho Brandao <marinho@gmail.com>
2012-05-30 00:50:32 +0100

    Improvements on assertation functions

--- djangoplus/test_utils.py
+++ djangoplus/test_utils.py
@@ -3,7 +3,8 @@ import os, unittest, doctest
 
 from django.core.serializers import deserialize
 from django.db.models import get_apps
-from django.test.simple import get_tests, run_tests as old_run_tests
+from django.test.simple import get_tests
+from django.core import management
 
 try:
     set
@@ -12,12 +13,14 @@ except:
 
 def load_fixture(path, file_type='json'):
     """Load a fixture file"""
-    fp = file(path)
-    cont = fp.read()
-    fp.close()
+    management.call_command('loaddata', path, verbosity=0)
 
-    for obj in deserialize(file_type, cont):
-        obj.save()
+    #fp = file(path)
+    #cont = fp.read()
+    #fp.close()
+    #
+    #for obj in deserialize(file_type, cont):
+    #    obj.save()
 
 def model_has_fields(model_class, fields):
     """Checks if a model class has all fields in fields list and returns a
@@ -30,6 +33,10 @@ def model_has_fields(model_class, fields):
             [f.name for f in model_class._meta.many_to_many]
             )
     return list(fields - model_fields)
+
+def assert_model_has_fields(model_class, fields):
+    if model_has_fields(model_class, fields):
+        return fields
 
 def is_model_class_fk(model_class_from, field, model_class_to):
     """Returns True if field is ForeignKey to model class informed"""
@@ -60,42 +67,6 @@ def is_field_type(model_class_from, field, field_type, **kwargs):
 def is_model_pk(model_class, field):
     """Checks if a field is the primary key of the model class"""
     return model_class._meta.pk.name == field
-
-def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
-    """Test runner to support many DocTests *.txt files and TestUnits *.py
-    using a setting TEST_FILES in app.tests module"""
-    for app in get_apps():
-        test_mod = get_tests(app)
-
-        if not test_mod or hasattr(test_mod, 'suite'):
-            continue
-
-        suites = []
-
-        # DocTest files
-        for filename in getattr(test_mod, 'DOCTEST_FILES', []):
-            try:
-                suites.append(doctest.DocFileSuite(
-                    filename,
-                    package=test_mod,
-                    encoding='utf-8',
-                    ))
-            except TypeError:
-                suites.append(doctest.DocFileSuite(
-                    filename,
-                    package=test_mod,
-                    ))
-
-        # Unit Tests modules
-        for module in getattr(test_mod, 'UNITTEST_MODULES', []):
-            suites.append(unittest.TestLoader().loadTestsFromModule(module))
-
-        # Sets the 'suites' attribute to test module
-        if suites:
-            print suites
-            test_mod.suite = lambda: unittest.TestSuite(suites)
-
-    return old_run_tests(test_labels, verbosity, interactive, extra_tests)
 
 def url_status_code(url, status_code=200, content=None, client=None, return_response=False):
     """Checks if the informed URL returns the wanted status_code"""
